93bbcbcbc16116fa0079f7b276bccc9ef12d103b
Julianne Vela <cjvela@outlook.com>
2021-03-03 19:42:11 -0800

    initial setup, file tree, app.js, and api-utils functions

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {
+	getLocalStorage,
+	setLocalStorage,
+} from './Components/Utils/ls-utils.js';
 import './App.css';
+import PrivateRoute from './Components/PrivateRoute.js';
+import Header from './Components/Header/Header.js';
+import HomePage from './HomePage/HomePage.js';
+import AuthPage from './AuthPage/AuthPage.js';
+import AccountDash from './AccountDash/AccountDash.js';
+import SearchPage from './SearchPage/SearchPage.js';
+import BookmarksPage from './BookmarksPage/BookmarksPage.js';
+export default class App extends Component {
+	state = {
+		token: getLocalStorage(),
+	};
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	handleTokenChange = (token) => {
+		this.setState({ token });
+
+		setLocalStorage(token);
+	};
+
+	render() {
+		const { token } = this.state;
+
+		return (
+			<Router>
+				<Header />
+				<Switch>
+					<Route
+						path='/'
+						exact
+						token={token}
+						render={(routerProps) => <HomePage {...routerProps} />}
+					/>
+
+					<PrivateRoute
+						path='/myaccount/dashboard'
+						exact
+						token={token}
+						render={(routerProps) => (
+							<AccountDash {...routerProps} />
+						)}
+					/>
+
+					<Route
+						path='/myaccount/signup'
+						exact
+						token={token}
+						render={(routerProps) => <AuthPage {...routerProps} />}
+					/>
+					<Route
+						path='/myaccount/signin'
+						exact
+						token={token}
+						render={(routerProps) => <AuthPage {...routerProps} />}
+					/>
+
+					<PrivateRoute
+						path='/search'
+						exact
+						token={token}
+						render={(routerProps) => (
+							<SearchPage {...routerProps} />
+						)}
+					/>
+
+					<PrivateRoute
+						path='/bookmarks'
+						exact
+						token={token}
+						render={(routerProps) => (
+							<BookmarksPage {...routerProps} />
+						)}
+					/>
+				</Switch>
+			</Router>
+		);
+	}
 }
-
-export default App;
